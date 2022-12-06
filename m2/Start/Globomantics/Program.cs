93aae8d683d11113299514a526a7a88e026d1817
Roland Guijt <roland.guijt@gmail.com>
2022-12-06 09:53:29 +0100

    Finished and start

---
+++ m2/Start/Globomantics/Program.cs
@@ -0,0 +1,25 @@
+using Globomantics.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IConferenceRepository, ConferenceRepository>();
+builder.Services.AddScoped<IProposalRepository, ProposalRepository>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Conference}/{action=Index}/{id?}");
+});
+
+app.Run();
---
+++ m2/Start/Globomantics/Repositories/IProposalRepository.cs
@@ -0,0 +1,10 @@
+﻿using Globomantics.Models;
+
+namespace Globomantics.Repositories;
+
+public interface IProposalRepository
+{
+    int Add(ProposalModel model);
+    ProposalModel Approve(int proposalId);
+    IEnumerable<ProposalModel> GetAllForConference(int conferenceId);
+}
